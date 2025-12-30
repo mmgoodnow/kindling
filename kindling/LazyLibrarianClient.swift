a287b6c3cd65ce83c4deefe40196cb3d5b99462a
Michael Goodnow <mmgoodnow@gmail.com>
2025-12-30 16:59:05 -0500

    Log bad file download responses

--- kindling/LazyLibrarianClient.swift
+++ kindling/LazyLibrarianClient.swift
@@ -828,6 +828,11 @@ struct LazyLibrarianClient: LazyLibrarianServing {
     }
     let (tempURL, response) = try await session.download(from: url)
     guard let http = response as? HTTPURLResponse, (200..<300).contains(http.statusCode) else {
+      #if DEBUG
+        if let data = try? Data(contentsOf: tempURL) {
+          logResponse("getBookFileDirect bad response", data: data)
+        }
+      #endif
       throw LazyLibrarianError.badResponse
     }
     return try moveDownloadedEpub(from: tempURL)
@@ -847,6 +852,11 @@ struct LazyLibrarianClient: LazyLibrarianServing {
     }
     let (tempURL, response) = try await session.download(from: url)
     guard let http = response as? HTTPURLResponse, (200..<300).contains(http.statusCode) else {
+      #if DEBUG
+        if let data = try? Data(contentsOf: tempURL) {
+          logResponse("getFileDirect bad response", data: data)
+        }
+      #endif
       throw LazyLibrarianError.badResponse
     }
     return try moveDownloadedAudiobook(from: tempURL)
